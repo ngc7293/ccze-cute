f43577dac9c1c25b077a5ce031c866d56dfdefe7
David Bourgault <dav.bour@gmail.com>
2021-09-23 22:19:30 -0400

    Updated Regex, support logging::tag

--- src/cute.c
+++ src/cute.c
@@ -5,7 +5,7 @@
 
 #include <pcre.h>
 
-static const char* REGEX = "^\\[(?<date>[0-9\\- \\:]+)\\] \\((?<level>[a-z]+)\\)(?: <thread (?<thread>[0-9a-f]+)>)? \\[(?<component>[^\\]]+)\\] (?<message>.+)$";
+static const char* REGEX = "^\\[(?<date>[0-9T\\-:]+)\\] \\((?<level>[a-z]+)\\)(?: <thread (?<thread>[0-9a-f]+)>)? \\[(?<component>[a-zA-Z\\\\:]+)\\] (?<message>.+?)([^\\s=]+=[\\S]+)*\\s*$";
 static pcre* re;
 static pcre_extra* re_extra;
 
@@ -52,17 +52,17 @@ static int ccze_cute_handle(const char *str, size_t length, char **rest)
     const char* line;
 
     if (ret < 0) {
-        *rest = str;
+        *rest = NULL;
         return 0;
     }
 
-    if (pcre_get_named_substring(re, str, vector, ret, "date", &line)) {
+    if (pcre_get_named_substring(re, str, vector, ret, "date", &line) > 0) {
         ccze_addstr(CCZE_COLOR_STATIC_WHITE, "[");
         ccze_addstr(CCZE_COLOR_STATIC_YELLOW, line);
         ccze_addstr(CCZE_COLOR_STATIC_WHITE, "] ");
     }
 
-    if (pcre_get_named_substring(re, str, vector, ret, "level", &line)) {
+    if (pcre_get_named_substring(re, str, vector, ret, "level", &line) > 0) {
         ccze_color_t color = CCZE_COLOR_STATIC_WHITE;
         for (int i = 0; levels[i].level != NULL; i++) {
             if (strcmp(line, levels[i].level) == 0) {
@@ -76,20 +76,33 @@ static int ccze_cute_handle(const char *str, size_t length, char **rest)
         ccze_addstr(CCZE_COLOR_STATIC_WHITE, ") ");
     }
 
-    if (pcre_get_named_substring(re, str, vector, ret, "thread", &line)) {
+    if (pcre_get_named_substring(re, str, vector, ret, "thread", &line) > 0) {
         ccze_addstr(CCZE_COLOR_STATIC_WHITE, "<thread ");
         ccze_addstr(CCZE_COLOR_STATIC_BLUE, line + strlen(line) - 5);
         ccze_addstr(CCZE_COLOR_STATIC_WHITE, "> ");
     }
 
-    if (pcre_get_named_substring(re, str, vector, ret, "component", &line)) {
+    if (pcre_get_named_substring(re, str, vector, ret, "component", &line) > 0) {
         ccze_addstr(CCZE_COLOR_STATIC_WHITE, "[");
         addstr_pad(CCZE_COLOR_STATIC_GREEN, line, 12);
         ccze_addstr(CCZE_COLOR_STATIC_WHITE, "] ");
     }
 
-    if (pcre_get_named_substring(re, str, vector, ret, "message", &line)) {
+    if (pcre_get_named_substring(re, str, vector, ret, "message", &line) > 0) {
         ccze_addstr(CCZE_COLOR_STATIC_WHITE, line);
+    }
+
+    char tagbuf[128] = { 0 };
+    int n = 6, l = 0;
+    while ((l = pcre_get_substring(str, vector, ret, n++, &line)) > 0) {
+        strncpy(tagbuf, line, sizeof(tagbuf));
+        int epos = 0;
+        while (line[++epos] != '=');
+
+        tagbuf[epos] = 0;
+        ccze_addstr(CCZE_COLOR_STATIC_GREEN, tagbuf);
+        ccze_addstr(CCZE_COLOR_STATIC_BOLD_BLACK, "=");
+        ccze_addstr(CCZE_COLOR_STATIC_YELLOW, tagbuf + epos + 1);
     }
 
     ccze_newline();
